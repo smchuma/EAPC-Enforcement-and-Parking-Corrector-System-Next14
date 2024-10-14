fc35abea1d16dbd7b4493103e1a94b3de00c5bae
Samora Mchuma <laurentsamora6@gmail.com>
2024-10-14 16:50:08 +0300

    added extra styles to report

--- app/(taarifa)/tuma-ripoti/page.jsx
+++ app/(taarifa)/tuma-ripoti/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
       <h1 className="text-3xl text-center border-b-2 border-gray-300 pb-5">
         Taarifa za kujaza
       </h1>
-      <div className="flex items-center justify-center mt-8">
+      <div className="flex items-center justify-center bg-white py-12 rounded-lg shadow-md mx-4 lg:mx-0">
         <SalesForm />
       </div>
     </main>
